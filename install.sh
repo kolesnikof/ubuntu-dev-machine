2a0316dd84db588f4088e4976a3abbb460ffb947
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2015-08-17 20:33:24 -0300

    Fixing up script variables

--- install.sh
+++ install.sh
@@ -1,15 +1,33 @@
-echo -p "Do you want to install ruby development environment?" rubyEnv
-echo -p "Do you want to install node.js development environment?" nodeEnv
-echo -p "Do you want to install php development environment?" phpEnv
-echo -p "Would you like to install Google Chrome?" gChrome
-echo -p "Would you like to install Sublime Text 3?" sublime
-echo -p "Would you like to install MongoDB?" mongodb
-echo -p "Would you like to setup git?" setupGit
-echo -p "What password should we set in MySQL and PostgreSQL?" dbPassword
+#!/bin/bash
 
-if [ setupGit -eq "y" ]; then
-  echo -p "What is your name? " gitName
-  echo -p "What is your email? " gitEmail
+echo "******************************************************"
+echo "                 Ubuntu Dev Machine                   "
+echo "******************************************************"
+echo ""
+echo "I'll ask all the questions at this point, and then go take some rest..."
+
+echo -n "Do you want to setup ruby development environment?"
+read rubyEnv
+echo -n "Do you want to setup node.js development environment?"
+read nodeEnv
+echo -n "Do you want to setup php development environment?"
+read phpEnv
+echo -n "Would you like to install Google Chrome?"
+read gChrome
+echo -n "Would you like to install Sublime Text 3?"
+read sublime
+echo -n "Would you like to install MongoDB?"
+read mongodb
+echo -n "Would you like to setup git?"
+read setupGit
+echo -n "What password should we set in MySQL and PostgreSQL?"
+read dbPassword
+
+if [ $setupGit == "y" ]; then
+  echo -n "What is your name? "
+  read gitName
+  echo -n "What is your email? "
+  read gitEmail
 fi
 
 sudo -s <<HERE
@@ -23,68 +41,54 @@ sudo -s <<HERE
 
   apt-get install --yes build-essential gcc git imagemagick subversion curl zsh oracle-java8-installer phantomjs xpdf postgresql libxslt-dev libxml2-dev meld vim memcache virtualbox vagrant
   apt-get install oracle-java8-set-default --yes
-  
+
+  if [ $phpEnv == "y" ]; then
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password $(dbPassword)"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $(dbPassword)"
+
+    apt-get install --yes mysql-server mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
+
+    service apache2 restart
+
+    a2enmod rewrite headers deflate security
+
+    sed -i 's/diplay_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini
+    sed -i 's/diplay_startup_errors = Off/display_startup_errors = On/g' /etc/php5/apache2/php.ini
+    sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php5/apache2/php.ini
+
+    service apache2 restart
+  fi
+
 HERE
 
-curl -L http://install.ohmyz.sh | sh
-
-if [ rubyEnv -eq "y" ]; then
-  \curl -sSL https://get.rvm.io | bash -s stable
-
-  source ~/.rvm/scripts/rvm
-
-
-  rvm install 1.9.3 --disable-binary
-  rvm install 2.0.0 --disable-binary
-  rvm install 2.1.1 --disable-binary
-
-  rvm use 2.0.0 --default
-fi
-
-if [ nodeEnv -eq "y" ]; then
-
-  curl https://raw.githubusercontent.com/creationix/nvm/v0.12.2/install.sh | bash
-
-  source ~/.nvm/nvm.sh
-
-  nvm install 0.10
-  nvm use 0.10  
-  nvm alias default 0.10
-
-fi
-
-if [ phpEnv -eq "y" ]; then
-
-sudo -s <<HERE
-
-  debconf-set-selections <<< "mysql-server mysql-server/root_password password $dbPassword"
-  debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $dbPassword"
-
-  apt-get install --yes mysql-server mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
-
-  service apache2 restart
-
-  a2enmod rewrite headers deflate security
-
-  sed -i 's/diplay_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini
-  sed -i 's/diplay_startup_errors = Off/display_startup_errors = On/g' /etc/php5/apache2/php.ini
-  sed -i 's/memory_limit = 128M/memory_limit = 512M/g' /etc/php5/apache2/php.ini
-
-  service apache2 restart
-  
-HERE
-
-fi
-
-if [ mongodb -eq "y" ]; then
-
-
-
-fi
-
-if [ setupGit -eq "y" ]; then
+if [ $setupGit == "y" ]; then
   git config --global user.name $gitName
   git config --global user.email $gitEmail
 
   cat /dev/zero |  ssh-keygen -b 4096 -t rsa -C $gitEmail -q -N ""
+fi
+
+curl -L http://install.ohmyz.sh | sh
+
+if [ $rubyEnv == "y" ]; then
+  \curl -sSL https://get.rvm.io | bash -s stable
+
+  source ~/.rvm/scripts/rvm
+
+  rvm install 1.9.3 --disable-binary
+  rvm install 2.1.5 --disable-binary
+  rvm install 2.2 --disable-binary
+
+  rvm use 2.2 --default
+fi
+
+if [ $nodeEnv -eq "y" ]; then
+
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.0/install.sh | bash
+
+  source ~/.nvm/nvm.sh
+
+  nvm install iojs
+  nvm use iojs
+  nvm alias default iojs
 fi
