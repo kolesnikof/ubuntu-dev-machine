9f02dc84161323e6489a4a240048fd58c6007567
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2015-08-17 20:59:28 -0300

    Adding sublime text, google chrome, nodejs packages and more tweaks

--- install.sh
+++ install.sh
@@ -6,29 +6,36 @@ echo "******************************************************"
 echo ""
 echo "I'll ask all the questions at this point, and then go take some rest..."
 
-echo -n "Do you want to setup ruby development environment?"
+echo -n "Do you want to setup ruby development environment? "
 read rubyEnv
-echo -n "Do you want to setup node.js development environment?"
+echo -n "Do you want to setup node.js development environment? "
 read nodeEnv
-echo -n "Do you want to setup php development environment?"
+echo -n "Do you want to setup php development environment? "
 read phpEnv
-echo -n "Would you like to install Google Chrome?"
+echo -n "Would you like to install Google Chrome? "
 read gChrome
-echo -n "Would you like to install Sublime Text 3?"
+echo -n "Would you like to install Sublime Text 3? "
 read sublime
-echo -n "Would you like to install MongoDB?"
+echo -n "Would you like to install MongoDB? "
 read mongodb
-echo -n "Would you like to setup git?"
+echo -n "Would you like to setup git? "
 read setupGit
-echo -n "What password should we set in MySQL and PostgreSQL?"
+echo -n "What password should we set in MySQL and PostgreSQL? "
 read dbPassword
 
-if [ $setupGit == "y" ]; then
+if [ ${nodeEnv} == "y" ]; then
+  echo "Would you like some packages to be installed by default? (separated by comma) "
+  read nodePackages
+fi
+
+if [ ${setupGit} == "y" ]; then
   echo -n "What is your name? "
   read gitName
   echo -n "What is your email? "
   read gitEmail
 fi
+
+MACHINE_TYPE=`uname -m`
 
 sudo -s <<HERE
 
@@ -42,9 +49,28 @@ sudo -s <<HERE
   apt-get install --yes build-essential gcc git imagemagick subversion curl zsh oracle-java8-installer phantomjs xpdf postgresql libxslt-dev libxml2-dev meld vim memcache virtualbox vagrant
   apt-get install oracle-java8-set-default --yes
 
-  if [ $phpEnv == "y" ]; then
-    debconf-set-selections <<< "mysql-server mysql-server/root_password password $(dbPassword)"
-    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $(dbPassword)"
+  if [ ${gChrome} == "y"]; then
+    apt-get install libxss1 libappindicator1 libindicator7
+    apt-get install -f
+
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    else
+      wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+    fi
+
+    dpkg -i google-chrome*.deb
+  fi
+
+  if [ ${sublime} == "y" ]; then
+    add-apt-repository ppa:webupd8team/sublime-text-3
+    apt-get update
+    apt-get install sublime-text-installer
+  fi
+
+  if [ ${phpEnv} == "y" ]; then
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password ${dbPassword}"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${dbPassword}"
 
     apt-get install --yes mysql-server mysql-client apache2 php5 libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
 
@@ -61,7 +87,7 @@ sudo -s <<HERE
 
 HERE
 
-if [ $setupGit == "y" ]; then
+if [ ${setupGit} == "y" ]; then
   git config --global user.name $gitName
   git config --global user.email $gitEmail
 
@@ -70,7 +96,7 @@ fi
 
 curl -L http://install.ohmyz.sh | sh
 
-if [ $rubyEnv == "y" ]; then
+if [ ${rubyEnv} == "y" ]; then
   \curl -sSL https://get.rvm.io | bash -s stable
 
   source ~/.rvm/scripts/rvm
@@ -82,7 +108,7 @@ if [ $rubyEnv == "y" ]; then
   rvm use 2.2 --default
 fi
 
-if [ $nodeEnv -eq "y" ]; then
+if [ ${nodeEnv} == "y" ]; then
 
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.0/install.sh | bash
 
@@ -91,4 +117,9 @@ if [ $nodeEnv -eq "y" ]; then
   nvm install iojs
   nvm use iojs
   nvm alias default iojs
+
+  if [ ${nodePackages} ]; then
+    packages=${nodePackages//,/ }
+    npm install -g packages
+  fi
 fi
